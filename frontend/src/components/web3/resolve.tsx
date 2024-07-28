3a4887ccd64dac922229f870f768dd5c1cb96b63
Thang X. Vu <thang@coongcrafts.io>
2024-07-28 17:46:47 +0700

    feat: integrate dedot & resolve url

--- frontend/src/components/web3/resolve.tsx
+++ frontend/src/components/web3/resolve.tsx
@@ -1,65 +1,68 @@
-import {
-  contractQuery,
-  decodeOutput,
-  useInkathon,
-  useRegisteredContract,
-} from "@scio-labs/use-inkathon"
-import { useEffect, useMemo } from "react"
+import { useInkathon } from "@scio-labs/use-inkathon"
+import { useEffect, useMemo, useState } from "react"
 import Lottie from "react-lottie"
 import animationData from "../../assets/resolve.json"
-import { ContractIds } from "../../deployments/deployments"
 import { Footer } from "./footer"
 import toast from "react-hot-toast"
+import { DedotClient, WsProvider } from "dedot"
+import { Contract } from "dedot/contracts"
+import { LinkContractApi } from "@/contracts/link"
+import { hexToString } from "@polkadot/util"
 
 const DELAY = 6000
 
 export const Resolve: React.FC<{ slug: string }> = ({ slug }) => {
-  const { api } = useInkathon()
-  const { contract } = useRegisteredContract(ContractIds.Link)
-
-  const message = useMemo(() => {
-    if (!contract) return undefined
-    return contract.abi.findMessage("resolve")
-  }, [contract])
+  const { activeAccount, activeChain, deployments } = useInkathon();
+  const [client, setClient] = useState<DedotClient>();
 
   const mounted = useMemo(() => {
     return Date.now()
   }, [])
 
   useEffect(() => {
-    if (!contract || !message || !api) return
+    if (!activeChain) return;
 
-    contractQuery(
-      api,
-      "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY", // Alice
-      contract,
-      "resolve",
-      undefined,
-      [slug],
-    )
-      .then((outcome) => {
-        const output = decodeOutput(outcome, contract, "resolve")
-        if (output.isError) throw Error("Unable to query contract")
-        if (output.output) {
+    DedotClient.new(new WsProvider(activeChain.rpcUrls[0]))
+      .then(setClient);
+  }, [activeChain]);
+
+  useEffect(() => {
+    if (!client || !deployments || !activeChain) return;
+
+    (async () => {
+      const deployment = deployments.find(d => activeChain.network === d.networkId)!;
+
+      const contract = new Contract<LinkContractApi>(client, deployment.abi as any, deployment.address.toString());
+      const result = await contract.query.resolve(slug, { caller: activeAccount?.address || '5EeG3x2qiUMU8LkRz4WGyy9kFhLY3u1AQwZz9aidvis58jqj' });
+
+      if (result.isOk && result.data.isOk) {
+        if (result.data.value) {
+          const location = hexToString(result.data.value);
+          console.log('Resolved Location', location);
+
           if (mounted + DELAY < Date.now()) {
-            window.location = output.output as Location
+            window.location.href = location;
           } else {
             setTimeout(
               () => {
-                window.location = output.output as Location
+                window.location.href = location;
               },
               DELAY - (Date.now() - mounted),
             )
           }
         } else {
-          window.location = `/?slug=${slug}` as any as Location
+          toast.error(`No register url at slug: ${slug}`);
+          setTimeout(() =>{
+            window.location.href = `/?slug=${slug}`;
+          }, 2_000);
         }
-      })
-      .catch((e) => {
+      } else {
+        console.error(result);
         toast.error("Unable to resolve link")
-        console.log({ e })
-      })
-  }, [api, contract, message, mounted, slug])
+      }
+    })();
+  }, [client, deployments])
+
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center">
       <main className="flex h-screen w-screen flex-col items-center justify-center">
